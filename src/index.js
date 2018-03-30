d2648ec024a40807fe0389af2d324348671cc477
terngkub <terngnattapol@gmail.com>
2018-03-30 16:37:51 +0200

    add search bar (not finished)

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-	<Provider store={createStoreWithMiddleware(reducers)}>
-		<App />
-	</Provider>
+	<div className="container">
+		<Provider store={createStoreWithMiddleware(reducers)}>
+			<App />
+		</Provider>
+	</div>
 	, document.getElementById('root'));
