9dc70ea172b92ede875bb9e6b6a15a364f7dcf7d
terngkub <terngnattapol@gmail.com>
2018-03-31 16:52:52 +0200

    change some config for deploying

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const API_KEY = '85c79cc171ab27935ebf9413c47164a3';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
+
+	console.log(request);
 
 	return {
 		type: FETCH_WEATHER,
